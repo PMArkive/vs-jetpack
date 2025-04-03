fc057120a7bc65353bdf3a68074629375fcc2490
Ichunjo <ichunjo.le.terrible@gmail.com>
2025-04-04 01:26:01 +0200

    re-implement ClampScaler

--- vsscale/various.py
+++ vsscale/various.py
@@ -1,16 +1,15 @@
 from __future__ import annotations
 
-from dataclasses import dataclass
 from functools import partial
 from typing import TYPE_CHECKING, Any, Literal
 
 from vsexprtools import ExprOp, combine, complexpr_available, expr_func, norm_expr
-from vskernels import Catrom, Hermite, LinearScaler, Scaler, ScalerT
+from vskernels import Catrom, Hermite, KernelT, LinearScaler, Scaler, ScalerT
 from vsmasktools import ringing_mask
-from vsrgtools import RepairMode, box_blur, gauss_blur, repair
+from vsrgtools import RepairMode, box_blur, gauss_blur
 from vstools import (
-    ConstantFormatVideoNode, CustomOverflowError, PlanesT, VSFunctionNoArgs,
-    check_ref_clip, check_variable, check_variable_format, core, inject_self, scale_delta, vs
+    ConstantFormatVideoNode, CustomOverflowError, PlanesT, VSFunctionNoArgs, check_ref_clip, check_variable,
+    check_variable_format, core, inject_self, scale_delta, vs
 )
 
 from .generic import BaseGenericScaler, GenericScaler
@@ -22,73 +21,97 @@ __all__ = [
 ]
 
 
-@dataclass
 class ClampScaler(GenericScaler):
     """Clamp a reference Scaler."""
 
-    base_scaler: ScalerT
-    """Scaler to clamp."""
+    def __init__(
+        self,
+        base_scaler: ScalerT,
+        reference: ScalerT | vs.VideoNode,
+        strength: int = 80,
+        overshoot: float | None = None,
+        undershoot: float | None = None,
+        limit: RepairMode | bool = True,
+        operator: Literal[ExprOp.MAX, ExprOp.MIN] | None = ExprOp.MIN,
+        masked: bool = True,
+        *,
+        kernel: KernelT = Catrom,
+        scaler: ScalerT | None = None,
+        shifter: KernelT | None = None,
+        **kwargs: Any
+    ) -> None:
+        """
+        :param base_scaler:     Scaler to clamp.
+        :param reference:       Reference Scaler used to clamp base_scaler
+        :param strength:        Strength of clamping. Default to 80. Must be between 0 and 100 (exclusive)
+        :param overshoot:       Overshoot threshold within the 0.0 - 1.0 range.
+        :param undershoot:      Undershoot threshold within the 0.0 - 1.0 range.
+        :param limit:           Whether to use under/overshoot limit (True) or a reference repaired clip for limiting.
+        :param operator:        Whether to take the brightest or darkest pixels in the merge. Defaults to ExprOp.MIN.
+        :param masked:          Whether to mask with a ringing mask or not. Defaults to True
+        :param kernel:          Base kernel to be used for certain scaling/shifting/resampling operations.
+                                Defaults to Catrom.
+        :param scaler:          Scaler used for scaling operations. Defaults to kernel.
+        :param shifter:         Kernel used for shifting operations. Defaults to scaler.
+        """
+        self.base_scaler = Scaler.ensure_obj(base_scaler, self.__class__)
 
-    reference: ScalerT | vs.VideoNode
-    """Reference Scaler used to clamp base_scaler"""
+        self.reference: Scaler | vs.VideoNode
 
-    strength: int = 80
-    """Strength of clamping."""
+        if not isinstance(reference, vs.VideoNode):
+            self.reference = Scaler.ensure_obj(reference, self.__class__)
+        else:
+            self.reference = reference
 
-    overshoot: float | None = None
-    """Overshoot threshold."""
+        if not 0 < strength < 100:
+            raise CustomOverflowError("`strength` must be between 0 and 100 (exclusive)!", self.__class__)
 
-    undershoot: float | None = None
-    """Undershoot threshold."""
+        self.strength = strength
 
-    limit: RepairMode | bool = True
-    """Whether to use under/overshoot limit (True) or a reference repaired clip for limiting."""
-
-    operator: Literal[ExprOp.MAX, ExprOp.MIN] | None = ExprOp.MIN
-    """Whether to take the brightest or darkest pixels in the merge."""
-
-    masked: bool = True
-    """Whether to mask with a ringing mask or not."""
-
-    def __post_init__(self) -> None:
-        super().__post_init__()
-
-        if self.strength >= 100:
-            raise CustomOverflowError('strength can\'t be more or equal to 100!', self.__class__)
-        elif self.strength <= 0:
-            raise CustomOverflowError('strength can\'t be less or equal to 0!', self.__class__)
-
-        if self.overshoot is None:
+        if overshoot is None:
             self.overshoot = self.strength / 100
-        if self.undershoot is None:
+        else:
+            self.overshoot = overshoot
+
+        if undershoot is None:
             self.undershoot = self.overshoot
+        else:
+            self.undershoot = undershoot
 
-        self._base_scaler = self.ensure_scaler(self.base_scaler)
-        self._reference = None if isinstance(self.reference, vs.VideoNode) else self.ensure_scaler(self.reference)
+        self.limit = limit
+        self.operator = operator
+        self.masked = masked
 
-    @inject_self
-    def scale(  # type: ignore
-        self, clip: vs.VideoNode, width: int | None = None, height: int | None = None,
-        shift: tuple[float, float] = (0, 0), *, smooth: vs.VideoNode | None = None, **kwargs: Any
-    ) -> vs.VideoNode:
+        super().__init__(None, kernel=kernel, scaler=scaler, shifter=shifter, **kwargs)
+
+    @inject_self.cached
+    def scale(
+        self,
+        clip: vs.VideoNode,
+        width: int | None = None,
+        height: int | None = None,
+        shift: tuple[float, float] = (0, 0),
+        **kwargs: Any
+    ) -> ConstantFormatVideoNode:
+
         width, height = self._wh_norm(clip, width, height)
 
-        assert (self.undershoot or self.undershoot == 0) and (self.overshoot or self.overshoot == 0)
-
-        ref = self._base_scaler.scale(clip, width, height, shift, **kwargs)
+        base = self.base_scaler.scale(clip, width, height, shift, **kwargs)
 
         if isinstance(self.reference, vs.VideoNode):
             smooth = self.reference
 
             if shift != (0, 0):
-                smooth = self._kernel.shift(smooth, shift)
+                smooth = self.kernel.shift(smooth, shift)
         else:
-            assert self._reference
-            smooth = self._reference.scale(clip, width, height, shift)
+            smooth = self.reference.scale(clip, width, height, shift)
 
-        assert smooth
+        check_ref_clip(base, smooth)
 
-        check_ref_clip(ref, smooth)
+        if TYPE_CHECKING:
+            from vstools import check_variable_format
+            assert check_variable_format(base, self.__class__)
+            assert check_variable_format(smooth, self.__class__)
 
         merge_weight = self.strength / 100
 
@@ -96,35 +119,37 @@ class ClampScaler(GenericScaler):
             expression = 'x {merge_weight} * y {ref_weight} * + a {undershoot} - z {overshoot} + clip'
 
             merged = norm_expr(
-                [ref, smooth, smooth.std.Maximum(), smooth.std.Minimum()],
-                expression, merge_weight=merge_weight, ref_weight=1.0 - merge_weight,
+                [base, smooth, smooth.std.Maximum(), smooth.std.Minimum()],
+                expression,
+                merge_weight=merge_weight,
+                ref_weight=1.0 - merge_weight,
                 undershoot=scale_delta(self.undershoot, 32, clip),
                 overshoot=scale_delta(self.overshoot, 32, clip),
                 func=self.__class__
             )
         else:
-            merged = smooth.std.Merge(ref, merge_weight)
+            merged = smooth.std.Merge(base, merge_weight)
 
             if isinstance(self.limit, RepairMode):
-                merged = repair(merged, smooth, self.limit)
+                merged = self.limit(merged, smooth)
 
         if self.operator is not None:
-            merge2 = combine([smooth, ref], self.operator)
+            merge2 = combine([smooth, base], self.operator)
 
             if self.masked:
                 merged = merged.std.MaskedMerge(merge2, ringing_mask(smooth))
             else:
                 merged = merge2
         elif self.masked:
-            merged.std.MaskedMerge(smooth, ringing_mask(smooth))
+            merged = merged.std.MaskedMerge(smooth, ringing_mask(smooth))
 
         return merged
 
-    @property
-    def kernel_radius(self) -> int:  # type: ignore[override]
-        if self._reference:
-            return max(self._reference.kernel_radius, self._base_scaler.kernel_radius)
-        return self._base_scaler.kernel_radius
+    @inject_self.cached.property
+    def kernel_radius(self) -> int:
+        if not isinstance(self.reference, vs.VideoNode):
+            return max(self.reference.kernel_radius, self.base_scaler.kernel_radius)
+        return self.base_scaler.kernel_radius
 
 
 class DPID(BaseGenericScaler):
